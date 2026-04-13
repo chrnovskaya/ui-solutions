3a2ef3c30a24652539cad269670cb182d2de3f3b
Pavlo Sambur <samburp@gmail.com>
2026-04-13 18:47:27 +0300

    feat: add Tailwind CSS 4.2, refactor EventCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
-import EventCard from "./features/EventCard";
+import EventCard from "./features/event-card/EventCard";
 
 function App() {
-  return (
-    <div id="center">
-      <EventCard />
-    </div>
-  );
+    return (
+        <div id="center">
+            <EventCard count={9} desktop={3} mobile={2} />
+            <EventCard count={7} desktop={3} mobile={2} />
+            <EventCard count={5} desktop={3} mobile={2} />
+
+            <EventCard count={4} desktop={2} mobile={2} />
+            <EventCard count={32} desktop={6} mobile={4} />
+        </div>
+    );
 }
 
 export default App;
